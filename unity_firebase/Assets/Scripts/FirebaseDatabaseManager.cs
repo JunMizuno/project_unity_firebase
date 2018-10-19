10ee13499d02648e40159a132fdec206c354a57c
水野淳 <>
2018-10-19 09:52:38 +0900

    Firebaseからのデータの読み込みを実装

--- unity_firebase/Assets/Scripts/FirebaseDatabaseManager.cs
+++ unity_firebase/Assets/Scripts/FirebaseDatabaseManager.cs
@@ -9,7 +9,8 @@ public class FirebaseDatabaseManager : MonoBehaviour
 {
     private const string FIREBASE_ACCOUNT_URL = "https://testproject-e2271.firebaseio.com/";
     private const string FIREBASE_PARENT_PATH = "users";
-    private DatabaseReference _databaseReference;
+    private DatabaseReference databaseReference_;
+    private Firebase.Auth.FirebaseUser firebaseUser_;
 
     /// <summary>
     /// 開始時
@@ -22,7 +23,8 @@ public class FirebaseDatabaseManager : MonoBehaviour
         FirebaseApp.DefaultInstance.SetEditorDatabaseUrl(FIREBASE_ACCOUNT_URL);
 
         // テスト実行
-        WriteUserData();
+        //WriteUserData();
+        LoadUserData();
     }
 
     /// <summary>
@@ -56,7 +58,7 @@ public class FirebaseDatabaseManager : MonoBehaviour
     {
         // データベースのルートリファレンスを取得
         // 指定した場所までのパスを取得している
-        _databaseReference = FirebaseDatabase.DefaultInstance.GetReference(FIREBASE_PARENT_PATH);
+        databaseReference_ = FirebaseDatabase.DefaultInstance.GetReference(FIREBASE_PARENT_PATH);
 
         // @todo. 以下テスト用、実用的でなない
         var userData = new UserData(3);
@@ -67,7 +69,7 @@ public class FirebaseDatabaseManager : MonoBehaviour
         // 書き込むデータを設定
         // @memo. 同名のカラムが既に存在していた場合は上書きされる模様
         // @memo. この場合の「test02」などは仮ユーザーID、正式な場合はUDIUなどを組み合わせて生成させたものを指定することが望ましい
-        _databaseReference.Child("test03").SetRawJsonValueAsync(json);
+        databaseReference_.Child("test03").SetRawJsonValueAsync(json);
     }
 
     /// <summary>
@@ -76,6 +78,37 @@ public class FirebaseDatabaseManager : MonoBehaviour
     /// </summary>
     private void LoadUserData()
     {
+        // データベースのルートリファレンスを取得
+        // 指定した場所までのパスを取得している
+        databaseReference_ = FirebaseDatabase.DefaultInstance.GetReference(FIREBASE_PARENT_PATH + "/test02");
+
         // @todo. 以下テスト用、実用的ではない
+        databaseReference_.GetValueAsync().ContinueWith(task =>
+        {
+            if (task.IsFaulted)
+            {
+                Debug.Log("<color=red>" + "データ取得エラー" + "</color>");
+            }
+            else if (task.IsCompleted)
+            {
+                DataSnapshot snapShot = task.Result;
+
+                // jsonデータを取得する場合
+                string json = snapShot.GetRawJsonValue();
+                Debug.Log("<color=yellow>" + "json:" + json + "</color>");
+                UserData userData = JsonUtility.FromJson<UserData>(json);
+                Debug.Log("<color=yellow>" + "UserIndex:" + userData.userIndex + " DeviceIndex:" + userData.deviceIndex + " IsNew:" + userData.isNew + " UserName:" + userData.userName + "</color>");
+
+                // ループしてデータを取得する場合
+                // そのままのデータを使用すると文字列が文字化け(エスケープシーケンスによるもの)していたので下記の様な記述にしています
+                IEnumerable<DataSnapshot> result = snapShot.Children;
+                foreach (DataSnapshot data in result)
+                {
+                    string key = System.Text.RegularExpressions.Regex.Unescape(data.Key);
+                    string child = System.Text.RegularExpressions.Regex.Unescape(data.GetRawJsonValue());
+                    Debug.Log("<color=green>" + "dataのキー:" + key + " dataのjson:" + child + "</color>");
+                }
+            }
+        });
     }
 }
